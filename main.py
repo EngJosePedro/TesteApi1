13bda95cfeb972860e56907aa2e88aafd0bf1d94
EngJosePedro <92191792+EngJosePedro@users.noreply.github.com>
2021-10-08 22:28:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from src.server.instance import server
 # importar controles do App
 from src.controller.books import *
 from src.GurobiProjetc.proj1 import *
+
+
+
 server.run()
 
 
-"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -15,4 +18,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello World beachhh"
-"""
